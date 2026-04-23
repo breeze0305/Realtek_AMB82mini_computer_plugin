c115e33ecd69806a903aa4da8f54fa8f5f2dde5c
breeze <benfeng99@gmail.com>
2026-04-23 21:51:01 +0800

    build: set amb_plugin as default PyInstaller output name

--- bash.sh
+++ bash.sh
@@ -1,9 +1,4 @@
-pyinstaller --onefile --console --add-data "CH341SER.EXE;." main.py
-
-pyinstaller --onefile --console --add-data "CH341SER.EXE;." --add-data "lang/zh_TW.json;lang" main.py
-pyinstaller --onefile --console --add-data "CH341SER.EXE;." --add-data "lang/zh_TW.json;lang" --add-data "lang/en_US.json;lang" --add-data "lang/ja_JP.json;lang"  main.py
-
-pyinstaller --onefile --console --icon="./icon.ico" --add-data "CH341SER.EXE;." --add-data "lang/zh_TW.json;lang" --add-data "lang/en_US.json;lang" --add-data "lang/ja_JP.json;lang" --add-data "gesture_recognition/hand_code.txt;gesture_recognition" --add-data "gesture_recognition/hand_weight.nb;gesture_recognition" --add-data "image_classification_japan/img_class_cnn.nb;image_classification_japan" main.py
+pyinstaller --onefile --console --name amb_plugin --icon="./icon.ico" --add-data "CH341SER.EXE;." --add-data "lang/zh_TW.json;lang" --add-data "lang/en_US.json;lang" --add-data "lang/ja_JP.json;lang" --add-data "gesture_recognition/hand_code.txt;gesture_recognition" --add-data "gesture_recognition/hand_weight.nb;gesture_recognition" --add-data "image_classification_japan/img_class_cnn.nb;image_classification_japan" main.py
 
 print("\033[31m紅色字\033[0m")
 print("\033[32m綠色字\033[0m")
--- main.spec
+++ main.spec
@@ -22,7 +22,7 @@ exe = EXE(
     a.binaries,
     a.datas,
     [],
-    name='main',
+    name='amb_plugin',
     debug=False,
     bootloader_ignore_signals=False,
     strip=False,
